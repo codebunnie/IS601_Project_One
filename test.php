88f8757a061b7f8fe862808b485513547b89ee41
codebunnie <31676798+codebunnie@users.noreply.github.com>
2017-10-22 22:55:11 -0400

    First Upload

---
+++ test.php
@@ -0,0 +1,166 @@
+<?php
+
+
+
+
+if($_FILES['file']['type']!='application/vnd.ms-excel'){
+	echo "Wrong File type. Only excel files are allowed to upload. Valid file is '<b>example.xls</b>'. ";
+	echo "<a href='test.php.php'>Import Excel File</a>";
+	die;
+	}
+	if(move_uploaded_file($_FILES['file']['tmp_name'],$_FILES['file']['name']))
+		$filename = $_FILES['file']['name'];
+	else{
+		header('Location: test.php?error=true');
+		echo "Couldn't upload file, Please close the file while importing the file!!";
+		echo "<a href='test.php'>Import CSV File</a>";
+		die;
+	}
+
+
+
+
+$file = fopen($filename,"r"); //get the csv file
+echo "You are Uploading the Following File ".$filename;
+//print_r(fgetcsv($file));
+$data = fgetcsv($file);
+
+//echo "<br><br><br><br><br><br><br>";
+//$headrows = array(fgetcsv($file));
+
+echo "<br><br><br><br><br><br><br>";
+
+
+
+
+
+////print_r($headrows[0]);
+
+//echo "<br><br><br><br><br><br><br>";
+ //counter for while loop and number of columns
+ //echo "table headers <br>";
+
+// print csv headers
+/*
+while ($line = fgetcsv($file)){
+
+  // count($line) is the number of columns
+  $numcols = count($line);
+
+  // Bail out of the loop if columns are incorrect
+  if ($numcols != $allowedColNum) {
+     break;
+  }
+  $col = $line[0]; 
+  echo $batchcount++.". ".$col."\n";
+}
+*/
+
+
+
+
+?>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<table class="redTable">
+
+
+<?php
+
+
+//print headers
+ $numcols = count($data) - 1;
+ echo "Number of Table Columns is: ".$numcols;
+ 
+ echo "<thead><tr>";
+ 
+ $i = 0;
+ 
+ while ($i<=$numcols){
+	 
+			echo "<th><b>I=".$i."</b> LOOP 2 <br>".$data[$i]."</th>";
+			$i++;
+ }
+	echo "</tr></thead>";
+	$i = 0;
+			
+			
+ 
+
+ while (($data = fgetcsv($file)) !== FALSE) { //first while - while not end of file
+
+ //$data = rtrim($data);
+ echo "<tr>";
+ while ($i <= $numcols && $data[$i] !== NULL) 
+				{
+					echo "<td><b>I=".$i."</b> LOOP 2 <br>".$data[$i]."</td>";
+				$i ++;
+				}
+			    $i = 0;
+		echo "</tr>";
+
+			
+}
+
+ 
+
+/* 
+ echo "<thead><tr><th>".$data[0] ."</th>".
+	"<th>".$data[1]."</th>".
+	"<th>".$data[2]."</th>".
+	"<th>".$data[3]."</th>".
+	"<th>".$data[4]."</th>".
+	"<th>".$data[5]."</th>".
+	"<th>".$data[6]."</th>".
+	"<th>".$data[7]."</th>".
+	"<th>".$data[8]."</th>".
+	"<th>".$data[9]."</th>".
+	"<th>".$data[10]."</th>".
+	"<th>".$data[11]."</th>".
+	"<th>".$data[12]."</th>".
+	"<th>".$data[13]."</th>".
+	"<th>".$data[14]."</th>".
+	"<th>".$data[15]."</th>".
+	"<th>".$data[16]."</th>".	
+	"<th>".$data[17]."HEADER END</th> </tr></thead>".
+	"<br><br><br><br><br><br><br><br><br>";
+	
+	//get data 
+ 
+ */
+ 
+ 
+ 
+
+/*
+while (($data = fgetcsv($file)) !== FALSE) 
+
+ {
+    echo "<tr><td>".$data[0] ."</td>".
+	"<td>".$data[1]."</td>".
+	"<td>".$data[2]."</td>".
+	"<td>".$data[3]."</td>".
+	"<td>".$data[4]."</td>".
+	"<td>".$data[5]."</td>".
+	"<td>".$data[6]."</td>".
+	"<td>".$data[7]."</td>".
+	"<td>".$data[8]."</td>".
+	"<td>".$data[9]."</td>".
+	"<td>".$data[10]."</td>".
+	"<td>".$data[11]."</td>".
+	"<td>".$data[12]."</td>".
+	"<td>".$data[13]."</td>".
+	"<td>".$data[14]."</td>".
+	"<td>".$data[15]."</td>".
+	"<td>".$data[16]."</td>".	
+	"<td>".$data[17]."</td> </tr>";
+	
+}
+*/
+	echo "</table></html>";
+
+	fclose($file);
+	
+?>
---
+++ upload_form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>IS 601 - Gabrielle Mack - Project 1</title>
+<link rel="stylesheet" type="text/css" href="tableDev.css" media="all">
+</head>
+
+<body>
+
+
+<div id="SuperContainer">
+<div id="Header"><img src="logo.png" height="100" /></div>
+
+
+<div id="Container">
+<?php if(isset($_GET['error']))if($_GET['error'])echo "Error: Close your file before import";?>
+
+<form action="test.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
+  <label for="file">Browse to chose file</label>
+  <input type="file" name="file" id="file" />
+  <input type="submit" name="Import" value="Import" />
+    <h4>Please Note: Files must be in <b>.CSV</b> format only.</h4>
+</form>
+
+</div>
+<!-- End Container -->
+<div id="Footer"></div><!-- End Footer -->
+</div>
+<!-- End SuperContainer -->
+</body>
+</html>
