9100385ab93aec81f7ada0000e3cdfc9567c2b7c
codebunnie <31676798+codebunnie@users.noreply.github.com>
2017-10-22 23:58:25 -0400

    Cleanup
    Doing away with debugging statements

--- index.php
+++ index.php
@@ -4,30 +4,26 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 <title>IS 601 - Gabrielle Mack - Project 1</title>
-<link rel="stylesheet" type="text/css" href="tableDev.css" media="all">
+
 </head>
 
 <body>
 
 
-<div id="SuperContainer">
-<div id="Header"><img src="logo.png" height="100" /></div>
 
 
-<div id="Container">
+
+<div>
 <?php if(isset($_GET['error']))if($_GET['error'])echo "Error: Close your file before import";?>
-<?PHP
-echo  "TEMP DIRECTORY IS: ". sys_get_temp_dir(); 
 
-?>
-<form action="test.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
+<form action="upload.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
   <label for="file">Browse to chose file</label>
   <input type="file" name="file" id="file" />
   <input type="submit" name="Import" value="Import" />
     <h4>Please Note: Files must be in <b>.CSV</b> format only.</h4>
 	
 	<?PHP
-echo  "TEMP DIRECTORY IS: ". sys_get_temp_dir(); 
+echo  "SYSTEM TEMP DIRECTORY IS: ". sys_get_temp_dir()."br>"; 
 
 ?>
 </form>
---
+++ upload.php
@@ -0,0 +1,80 @@
+<?php
+
+
+if($_FILES['file']['type']!='application/vnd.ms-excel'){
+	echo "Wrong File type. Only CSV files are allowed to upload. Valid file is '<b>example.CSV</b>'. ";
+	echo "<a href='index.php'>Import CSV File</a>";
+	die;
+	}
+	if(move_uploaded_file($_FILES['file']['tmp_name'],$_FILES['file']['name']))
+		$filename = $_FILES['file']['name'];
+	else{
+		header('Location: index.php?error=true');
+		echo "Couldn't upload file, Please close the file while importing the file!!";
+		echo "<a href='index.php'>Import CSV File</a>";
+		die;
+	}
+
+
+$file = fopen($filename,"r"); //get the csv file
+echo "You are Uploading the Following File ".$filename."<br>";
+//print_r(fgetcsv($file));
+$data = fgetcsv($file);
+
+
+
+
+
+?>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<table class="redTable">
+
+
+<?php
+
+
+//print headers
+ $numcols = count($data) - 1;
+
+ 
+ echo "<thead><tr>";
+ 
+ $i = 0;
+ 
+ while ($i<=$numcols){
+	 
+			echo "<th>".$data[$i]."</th>";
+			$i++;
+ }
+	echo "</tr></thead>";
+	$i = 0;
+			
+			
+ 
+
+ while (($data = fgetcsv($file)) !== FALSE) { //first while - while not end of file
+
+ //$data = rtrim($data);
+ echo "<tr>";
+ while ($i <= $numcols && $data[$i] !== NULL) 
+				{
+					echo "<td>".$data[$i]."</td>";
+				$i ++;
+				}
+			    $i = 0;
+		echo "</tr>";
+
+			
+}
+
+ 
+
+
+	echo "</table></html>";
+
+	fclose($file);
+	
+?>
