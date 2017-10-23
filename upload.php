5c7324015e28c1cae99b99ee7891f6562b9aee70
codebunnie <31676798+codebunnie@users.noreply.github.com>
2017-10-23 15:55:11 -0400

    Directory Changes

--- upload.php
+++ upload.php
@@ -15,7 +15,7 @@ if($_FILES['file']['type']!='application/vnd.ms-excel'){
 	
 	die;
 	}
-	$dir = '../uploads/';
+	$dir = '/uploads/';
 	if(move_uploaded_file($_FILES['file']['tmp_name'],$dir.$_FILES['file']['name']))
 	{
 	$user_file_name = $_FILES['file']['name'];		
